b0feddf9a93323b62cfa860b915f915f921bd033
Shaurya Sinha <shaurya157@gmail.com>
2016-09-16 00:47:45 -0700

    defined deep_dup in Array, Stack class, Queue class and incomplete Map class

---
+++ w1d4/w1d4.rb
@@ -0,0 +1,60 @@
+class Array
+  def deep_dup
+    result = []
+    self.each do |num|
+      result << (self.is_a?(Hash) ? num.deep_dup : num)
+    end
+    result
+  end
+end
+
+class Stack
+  def initialize
+    @stack = []
+  end
+
+  def add(el)
+    @stack << el
+  end
+
+  def remove
+    @stack.pop
+  end
+
+  def show
+    @stack.deep_dup
+  end
+end
+
+class Queue
+  def initialize
+    @stack = []
+  end
+
+  def enqueue(el)
+    @stack << el
+  end
+
+  def dequeue
+    @stack.shift
+  end
+
+  def show
+    @stack.deep_dup
+  end
+end
+
+class Map
+  def initialize
+    @map = []
+  end
+
+  def lookup(key)
+    @map.find {|k| k= == key}
+  end
+
+  def assign(key, value)
+    index = lookup(key)
+    index ? @map[index][1] = value : @map << [key, value]
+  end
+end

aeeaa08e4fe11e424ee4d8a970b00d6186ddcb15
Shaurya Sinha <shaurya157@gmail.com>
2016-09-20 21:10:37 -0700

    just added w2d3 homework, beggining work

---
+++ w2d3/rspec-homework/spec/dessert_spec.rb
@@ -0,0 +1,39 @@
+require 'rspec'
+require 'dessert'
+
+=begin
+Instructions: implement all of the pending specs (the `it` statements without blocks)!
+ Be sure to look over the solutions when you're done.
+=end
+
+describe Dessert do
+  let(:brownie) { Dessert.new("brownie", 100) }
+
+  describe "#initialize" do
+    it "sets a type" do
+      expect(brownie.type).to eq("brownie")
+    end
+
+    it "sets a quantity"
+
+    it "starts ingredients as an empty array"
+
+    it "raises an argument error when given a non-integer quantity" do
+      expect { Dessert.new("cake", "tons") }.to raise_error(ArgumentError)
+    end
+  end
+
+  describe "#add_ingredient" do
+    it "adds an ingredient to the ingredients array"
+  end
+
+  describe "#mix!" do
+    it "shuffles the ingredient array"
+  end
+
+  describe "#eat" do
+    it "subtracts an amount from the quantity"
+
+    it "raises an error if the amount is greater than the quantity"
+  end
+end

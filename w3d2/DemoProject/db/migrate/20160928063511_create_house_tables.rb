e7e5e52af1aecfa285a78d429b812fcdcb89ad1e
Shaurya Sinha <shaurya157@gmail.com>
2016-09-28 07:19:10 -0700

    all rails homework done

---
+++ w3d2/DemoProject/db/migrate/20160928063511_create_house_tables.rb
@@ -0,0 +1,10 @@
+class CreateHouseTables < ActiveRecord::Migration
+  def change
+    create_table :houses do |t|
+      t.string :name
+      t.string :address
+      
+      t.timestamps
+    end
+  end
+end
--- w3d2/DemoProject/db/seeds.rb
+++ w3d2/DemoProject/db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+House.create(name: 'Apartment')
+House.create(name: 'Suburb')
+House.create(name: 'Beach House')
+
+People.create(name: 'Person3', house_id: 3)
+People.create(name: 'Person4', house_id: 2)
+People.create(name: 'Person5', house_id: 1)

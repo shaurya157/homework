835070632a3ba5dad1081db15e40720e97d250f9
Shaurya Sinha <shaurya157@gmail.com>
2016-10-06 09:33:19 -0700

    auth token idiot proof - removed ruby part on line 31 in appcontroller

--- w4d3/app/controllers/application_controller.rb
+++ w4d3/app/controllers/application_controller.rb
@@ -28,7 +28,7 @@ class ApplicationController < ActionController::Base
   def auth_token
     "<input type='hidden'
            name='authenticity_token'
-           value='<%= #{form_authenticity_token} %>'>".html_safe
+           value='#{form_authenticity_token}'>".html_safe
   end
 
 end
